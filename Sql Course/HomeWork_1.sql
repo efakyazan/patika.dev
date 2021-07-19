b789cdc939610b02423ae1da72c20c549a9ac2c4
efakyazan <ef_akyazan@hotmail.com>
2021-07-19 20:01:34 +0300

    Update and rename HomeWork_1 to HomeWork_1.sql

---
+++ Sql Course/HomeWork_1.sql
@@ -0,0 +1,7 @@
+Perform the following query scenarios on the dvdrental sample database.
+
+1- Sort the data in the title and description columns in the movie table.
+---
+SELECT TITLE,
+	DESCRIPTION
+FROM FILM
